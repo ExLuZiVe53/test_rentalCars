22209435938ff1ab452ab89cd1bec399c885859b
ExLuZiVe53 <imstudent27@gmail.com>
2024-02-16 21:26:27 +0100

    added all components

---
+++ src/components/CarCard/CarCard.jsx
@@ -0,0 +1,41 @@
+import { FcLike } from 'react-icons';
+
+const CarCard = ({
+  id,
+  year,
+  make,
+  model,
+  type,
+  img,
+  description,
+  fuelConsumption,
+  engineSize,
+  accessories,
+  functionalities,
+  rentalPrice,
+  rentalCompany,
+  address,
+  rentalConditions,
+  mileage,
+}) => {
+  return (
+    <div>
+      <img>{img}</img>
+      <h3>
+        {make}
+        <span>{model}</span>
+      </h3>
+      <span>{rentalPrice}</span>
+
+      <p>{address}</p>
+      <p>{rentalCompany}</p>
+      <p>{type}</p>
+      <p>{model}</p>
+      <p>{id}</p>
+      <p>{functionalities}</p>
+      <button></button>
+    </div>
+  );
+};
+
+export default CarCard;
